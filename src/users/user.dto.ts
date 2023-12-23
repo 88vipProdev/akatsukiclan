89270047ebba34b447159effeada76968d349c13
88vipProdev <nguyentunglam18091998abc@gmail.com>
2023-12-23 18:29:10 +0700

    tdo

---
+++ src/users/user.dto.ts
@@ -0,0 +1,24 @@
+export class UserDto {
+
+
+        @Expose()
+        id : number
+
+  
+        fristName : string 
+
+  
+        middleName : string 
+
+      
+        lastName: string 
+
+      
+        sex :string
+        
+      
+        email :string 
+
+       
+        password: string 
+}
